893ff886a667789cd377e4311acffe11ff016ab4
Douglas Serrano <esdouglasserrano@gmail.com>
2025-12-20 22:50:41 -0800

    Complete Rework

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.Design;
-using System.Diagnostics;
-using System.Linq.Expressions;
-using System.Runtime.ExceptionServices;
+﻿Boot();
 Menu();
 
 void Menu()
@@ -19,12 +16,25 @@ void Menu()
         selection = Console.ReadLine();
     }
     Reset();
-    
+
     if (selection == "0")
     {
         Console.Clear();
-        Console.WriteLine("Type your first guess!\n);
+        Console.WriteLine("Type your first guess!\n");
         Play();
+    }
+
+    if (selection == "1")
+    {
+        Console.Clear();
+        Console.WriteLine("Type your first guess!");
+        PlayDev();
+    }
+
+    if (selection == "2")
+    {
+        Console.Clear();
+        Exit();
     }
 }
 
@@ -153,7 +163,7 @@ void Play()
             Win(tries);
             break;
         }
-        
+
         // After each guess subtract one from attempts
         attempts--;
     }
@@ -174,16 +184,47 @@ void Win(int tries)
     {
         Console.WriteLine($"Horray! You found the word in {tries} tries!");
     }
+    Console.ReadLine();
+    Console.Clear();
+    Menu();
 }
 
 void Lose(String word)
 {
     Console.WriteLine();
     Console.WriteLine($"You lost... The word was: {word}");
+    Console.ReadLine();
+    Console.Clear();
+    Menu();
 }
 
 
+void Exit()
+{
+    String message = "Goodbye!";
+    foreach (char c in message)
+    {
+        Console.Write(c);
+        Thread.Sleep(50);
+    }
+    Thread.Sleep(100);
+    Environment.Exit(0);
+}
 
+void Boot()
+{
+    Console.BackgroundColor = ConsoleColor.Blue;
+    Console.ForegroundColor = ConsoleColor.White;
+    String message = "AugeAgora";
+    foreach (char c in message)
+    {
+        Console.Write(c);
+        Thread.Sleep(50);
+    }
+    Thread.Sleep(100);
+    Reset();
+    Console.Clear();
+}
 
 
 
@@ -203,7 +244,7 @@ List<string> GetWordList()
     string filePath = @"valid-wordle-words.txt";
     List<string> wordList = new List<string>();
     wordList = File.ReadAllLines(filePath).ToList();
-    
+
     return wordList;
 }
 
@@ -272,4 +313,177 @@ void TitleColor(String s)
 void UserColor()
 {
     Console.ForegroundColor = ConsoleColor.Cyan;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This is literally just the same thing but with some things uncommented LMAO
+void PlayDev()
+{
+    // First we need to get the wordList and word
+    List<string> wordList = GetWordList();
+    String word = GetWord(wordList);
+    DevComment($"The word is: {word}");
+
+    List<string> guessesMade = new List<string>();
+    int attempts = 6;
+    int tries = 0;
+    while (attempts > 0)
+    {
+        // Add one try
+        tries++;
+
+        // Let the user guess
+        UserColor();
+        String guess = Console.ReadLine().ToLower();
+        // DevComment($"User's guess: {guess}");
+
+        while (wordList.Contains(guess) == false)
+        {
+            WarningComment("Invalid Guess");
+            DevComment("Word does not exist in: valid-wordle-words.txt");
+            guess = Console.ReadLine().ToLower();
+        }
+        while (guessesMade.Contains(guess))
+        {
+            WarningComment("You've already guessed that word!");
+            guess = Console.ReadLine().ToLower();
+        }
+        Reset();
+
+        // Add valid guess to guessesMade list
+        guessesMade.Add(guess);
+
+        // Create an array for the user's guess
+        String[] guessArray = { "", "", "", "", "" };
+        for (int i = 0; i < 5; i++)
+        {
+            guessArray[i] = guess[i].ToString();
+        }
+
+        // Count each letter in the word
+        Dictionary<char, int> letterCount = new Dictionary<char, int>();
+        foreach (char letter in word)
+        {
+            if (letterCount.ContainsKey(letter) == false)
+            {
+                letterCount.Add(letter, 0);
+            }
+            if (letterCount.ContainsKey(letter) == true)
+            {
+                letterCount[letter]++;
+            }
+        }
+
+        DevComment($"Letter count for word: {word}");
+        foreach (var element in letterCount)
+        {
+            DevComment(element.Key + ":" + element.Value);
+        }
+
+
+        // Check for green (correct letter position)
+        for (int i = 0; i < 5; i++)
+        {
+            if (guess[i] == word[i])
+            {
+                guessArray[i] = "Green";
+                letterCount[guess[i]]--;
+            }
+        }
+
+        // Check for gray (not in the word)
+        for (int i = 0; i < 5; i++)
+        {
+            if (word.Contains(guess[i]) == false)
+            {
+                guessArray[i] = "Gray";
+            }
+        }
+
+        // Check for yellow (exists in the word)
+        for (int i = 0; i < 5; i++)
+        {
+            if (word.Contains(guess[i]) && guessArray[i] != "Green" && guessArray[i] != "Gray" && letterCount[guess[i]] != 0)
+            {
+                guessArray[i] = "Yellow";
+                letterCount[guess[i]]--;
+            }
+            else if (word.Contains(guess[i]) && guessArray[i] != "Green" && guessArray[i] != "Gray" && letterCount[guess[i]] == 0)
+            {
+                guessArray[i] = "Repeat";
+            }
+        }
+
+        // Print out the guess with corresponding colors
+        for (int i = 0; i < 5; i++)
+        {
+            if (guessArray[i] == "Green")
+            {
+                Green($"{guess[i]}");
+            }
+            else if (guessArray[i] == "Gray")
+            {
+                Gray($"{guess[i]}");
+            }
+            else if (guessArray[i] == "Yellow")
+            {
+                Yellow($"{guess[i]}");
+            }
+            else if (guessArray[i] == "Repeat")
+            {
+                Gray($"{guess[i]}");
+            }
+        }
+        Console.WriteLine();
+
+        // Check to see if user has won
+        if (guess == word)
+        {
+            Win(tries);
+            break;
+        }
+
+        // After each guess subtract one from attempts
+        attempts--;
+    }
+    if (attempts == 0)
+    {
+        Lose(word);
+    }
 }
